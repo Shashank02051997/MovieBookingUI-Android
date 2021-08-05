86232a1a58433cf5584916b4c9e3eae87b9fda38
Shashank Singhal <shashanksinghal02@gmail.com>
2021-08-05 15:14:43 +0530

    - Upgraded the dependencies

--- app/src/main/java/com/minemark/platform/bookmovieappui/MainActivity.java
+++ app/src/main/java/com/minemark/platform/bookmovieappui/MainActivity.java
@@ -1,10 +1,8 @@
 package com.minemark.platform.bookmovieappui;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
